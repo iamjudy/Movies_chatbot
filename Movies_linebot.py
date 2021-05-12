94bb9fa75ba6eb627c59899a7c0083ae9c1e4a63
JudyChan <ctchanjudy@gmail.com>
2021-05-12 19:17:25 +0800

    try6

--- Movies_linebot.py
+++ Movies_linebot.py
@@ -17,42 +17,23 @@ df2=pd.read_csv('./tmdb_5000_movies.csv')
 df1.columns = ['id','tittle','cast','crew']
 df2= df2.merge(df1,on='id')
 
-
-
-
 # Compute the cosine similarity matrix
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-
-
 
 #Construct a reverse map of indices and movie titles
 indices = pd.Series(df2.index, index=df2['title']).drop_duplicates()
 
 
-
-
 def get_recommendations(title, cosine_sim=cosine_sim):
-    # Get the index of the movie that matches the title
     idx = indices[title]
-
-    # Get the pairwsie similarity scores of all movies with that movie
     sim_scores = list(enumerate(cosine_sim[idx]))
-
-    # Sort the movies based on the similarity scores
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
-    # Get the scores of the 10 most similar movies
     sim_scores = sim_scores[1:11]
 
-    # Get the movie indices
     movie_indices = [i[0] for i in sim_scores]
 
-    # Return the top 10 most similar movies
     return df2['title'].iloc[movie_indices]
-
-
-# ### Credits, Genres and Keywords Based Recommender
-# #### 詞袋模型
 
 
 
@@ -61,13 +42,11 @@ for feature in features:
     df2[feature] = df2[feature].apply(literal_eval)
 
 
-
 def get_director(x):
     for i in x:
         if i['job'] == 'Director':
             return i['name']
     return np.nan
-
 
 def get_list(x):
     if isinstance(x, list):
@@ -85,9 +64,6 @@ df2['director'] = df2['crew'].apply(get_director)
 features = ['cast', 'keywords', 'genres']
 for feature in features:
     df2[feature] = df2[feature].apply(get_list)
-
-
-df2[['title', 'cast', 'director', 'keywords', 'genres']].head(3)
 
 
 
